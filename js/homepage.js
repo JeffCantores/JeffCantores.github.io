dc039802a11890ad4bd6136b456ab2d90c52b0fd
Jefferson Cantores <jeffersoncantores13@gmail.com>
2021-05-25 13:03:00 +0800

    Updated index properties

--- js/homepage.js
+++ js/homepage.js
@@ -1,1 +1,10 @@
+$(window).scroll(function() {
+		$('#object').each(function(){
+		var imagePos = $(this).offset().top;
 
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+});
