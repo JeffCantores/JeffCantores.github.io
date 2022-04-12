fd10c1826833a9cba4d617b1b676c996f282f29c
Jefferson Cantores <jeffersoncantores13@gmail.com>
2022-04-12 22:28:39 +0800

    Updated portfolio (Used React)

---
+++ portfolio-ni-jeff/src/data.js
@@ -0,0 +1,45 @@
+//projects
+export const projects = [
+    {
+      title: "Genesis: A Blockchain-based Medical Record System",
+      subtitle: "FERN Stack + Hyperledger Fabric",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+];
+
+//skills 
+export const skills = [
+   
+];
+
+//testimonials 
+export const testimonials = [
+   
+];
---
+++ portfolio-ni-jeff/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      
+    },
+
+  },
+  plugins: [],
+}
