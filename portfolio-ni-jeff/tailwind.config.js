ccdf9ffd4c39d516dea2391bc788bd54fd68f198
Jefferson Cantores <jeffersoncantores13@gmail.com>
2023-06-06 13:24:36 +0800

    Portfolio update
    Added build

--- portfolio-ni-jeff/tailwind.config.js
+++ portfolio-ni-jeff/tailwind.config.js
@@ -1,9 +1,20 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./src/*.{html,js}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.neutral,
+      yellow: colors.amber,
+      blue: colors.blue
+    },
     extend: {},
   },
   plugins: [],
