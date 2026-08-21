1eebb52341ce69151354e26582e534339e084503
JeffCantores <jeffersoncantores13@gmail.com>
2026-08-22 00:34:32 +0800

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/JeffCantores.github.io/',
+    base: '/',
     plugins: [
         react(),
         tailwindcss(),
