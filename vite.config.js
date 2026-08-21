d0d915bd258ea30a939df8b905d131923102dd03
JeffCantores <jeffersoncantores13@gmail.com>
2026-08-21 23:43:39 +0800

    Updated files

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  build: {
-    outDir: 'build',
-  },
+    base: '/',
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    build: {
+        outDir: 'build',
+    },
 });
